d3ef126c607c6b33aa45c105b4b9be4d638fb493
vianney <vianney.denis@gmail.com>
2020-09-15 09:05:02 +0800

    Update 2020/09/15

--- Topic_1_intro.R
+++ Topic_1_intro.R
@@ -1,60 +1,66 @@
-#####################
-# Script in class 1 #
-#####################
+# Topic 1: First steps
 
 rm(list=ls())# clean all objects in memory
-setwd ('D:/Data Lab/Data Vianney/Class/R ecology/Class 2017-2018/data_class') # set up new wd
-
 
 # Check for update 
-
 if(!require(installr)) {
   install.packages("installr"); require(installr)} 
 updateR()
 
+# Package
 install.packages ("abc") # install the package 'abc'
 library ("abc") # load the package 'abc'
 
-### exercice 1a
-install.packages('vegan')
-library('vegan')
-### end exercice
-
+# Help
 ? median
 help (median) # help on median function
-median # basic use of the function	
 ?? median # help on all function with median in the description
 
+# Working directory
 getwd() # identify your current working directory
 ?setwd # set up a new working directory
 
-# q() # to quit R
+# Quit R
+# q() R # not run
 
+# List 
 ls() # all objects in memory
+a<-'corals' # create an object 'a' containing 'corals'
+b<-'are' # create an object 'b' containing 'are'
+c<-'cool'# create an object 'c' containing 'cool'
+ls # list objects 'a', 'b', 'c'
 rm(list=ls()) # clean all objects in memory
 # press CTRL +L to clean all your console
 
-# play with script editor in R and send command 'median' to R
-install.packages('rite')
-library(rite)
-rite() # send command 'median' 
-riteout() # send command 'median'. Try CTRL+ENT to send the command
+# Calculator
+3+2 # addition
+3-2 # substraction
+3*2 # multiplication
+3/2 # division
+3^3 # power
+log(2) # logarithm
+exp(2) # exponential
+(5 + 3) / 4 # define priority using () or {} 
+pi*4 # common function
 
-# Using riteout, change your working directory
-setwd ('D:/.../')
-read.table ("taiwan.txt",header = TRUE,sep="\t", dec=".")
-# same as following if we are not changing the working directory
-read.table ("D:/.../taiwan.txt",header = T, 	sep="\t", dec=".")
-# also same as:
-read.table("taiwan.txt", TRUE, "\t",".")
-# create and name object
-taiwan <- read.table ("taiwan.txt", 	header = TRUE, sep="\t", dec=".")
-taiwan
+# Reading
+library(readxl) # I am loading a package (library) allowing me to read .xls file
+read_excel('Data/reef_fish.xlsx') # I am reading my file
+fish<-read_excel('Data/reef_fish.xlsx') # I am importing my file in an object called 'fish'
+fish<-read.table('Data/reef_fish.txt', header=T, sep='\t', dec='.') # reading txt file
+?read.table
+fish<-read.table ("D:/.../Topic 1/Data/reef_fish.txt",header = TRUE,sep="\t", dec=".")# long version
+fish<-read.table("taiwan.txt", TRUE, "\t",".")# you can also shorten some part once you get use to it, but be careful when using this
+# fish<-read.table(file.choose (), header = TRUE,sep="\t", dec=".") # not run
 
-# you can locate the file by hand
-dat <- read.table (file.choose (), 	header = T, sep="\t", dec=".")
+# R Studio Environment
+fish<-read.table('Data/reef_fish.txt', header=T, sep='\t', dec='.')
+barplot(fish$richness, main="Top 10 reef fish Richness (Allen, 2000)", horiz=TRUE, names.arg=fish$country, cex.names=0.5, las=1)
 
-# you can also use read.delim(), read.csv(), and others functions
+# Random Assignment
+library(dplyr)
+list_student<-c('a','b','c','d','e')
+sample(list_student, 1, replace = TRUE)
 
 #######
 # END #
