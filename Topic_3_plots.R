2f2c707b405476718493cc2d35e8817a05321be4
vianney <vianney.denis@gmail.com>
2020-10-11 16:56:45 +0800

    2nd Update 2020/10/11

--- Topic_3_plots.R
+++ Topic_3_plots.R
@@ -37,10 +37,10 @@ plot(Petal.Length ~ Petal.Width, dat = iris,
      xlab = 'Petal width (cm)', 
      ylab = 'Petal length (cm)', 
      main = 'Petal width and length of iris flower',
-     pch = 19, cex=ratio, 
+     pch = 19, cex=2, 
      col = alpha(col.iris, 0.2)) # set symbol types and color 
 
-legend(x="bottomleft", pch= 19, cex=1.5, legend= c("versicolor","setosa", "virginica"), col=alpha(levels(as.factor(col.iris)),0.2))
+legend(x="bottomright", pch= 19, cex=1.5, legend= c("versicolor","setosa", "virginica"), col=alpha(levels(as.factor(col.iris)),0.2))
        
 
 plot(Petal.Length ~ Petal.Width, dat = iris,
@@ -219,4 +219,245 @@ xyplot(Sepal.Length + Sepal.Width + Petal.Width ~ Petal.Length  | variety+Specie
 trellis.par.get()
 
 
+## The `ggplot` package
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+library(stringr)
+
+dat1 <- read.csv("Data/FAO_grains_NA.csv", header=TRUE)
+
+dat1w <- dat1 %>%
+   filter(Information == "Yield (Hg/Ha)", 
+          Country =="United States of America", 
+          Crop %in% c("Oats", "Maize", "Barley", "Buckwheat","Rye")) %>% 
+   select (Year, Crop, Value) %>% spread(key = Crop, value="Value")
+
+dat1l <- gather(dat1w, key = "Crop", value = "Yield", 2:6)
+
+dat1l2 <- dat1 %>%
+   filter(Information == "Yield (Hg/Ha)", 
+          Crop       %in% c("Oats", "Maize", "Barley", "Buckwheat","Rye")) %>%
+   select( Year, Crop, Country,  Yield = Value)  # Note that we are renaming the Value field
+
+dat2 <- read.csv("Data/Income_education.csv", header=TRUE)
+
+Edu.Gend <- data.frame( 
+   variable   = paste("B200040", str_pad(1:18, width=2 , pad="0"),sep="" ),
+   Level      = rep(c("All", "NoHS","HS","AD","BD","Grad"), times=3),
+   Gender     = rep(c("All", "M","F"), each=6) )
+
+
+st.reg <- data.frame(State = tolower(state.abb), Region = state.region)# Create a region/state join table
+st.reg <- rbind(st.reg , data.frame(State="dc", Region="South") )
+
+dat2b <- dat2 %>% # Start the piping operations
+   gather(key = "variable", value = "value", -1:-2)  %>%
+   left_join(Edu.Gend, by="variable" )              %>%
+   select(State, County, Level, Gender, value)       %>%
+   mutate(Level = factor(Level, levels = c("All","NoHS","HS", "AD", "BD", "Grad"))) %>%
+   left_join(st.reg , by="State")
+
+dat2c <- spread(dat2b, key = Gender, value = value )
+
+ggplot(dat1l , aes(x = Year, y = Yield)) + geom_point()
+ggplot(dat1l , aes(x = Year, y = Yield, color = Crop)) + geom_point()
+ggplot(dat1l , aes(x = Year, y = Yield, color = Crop)) + geom_line()
+ggplot(dat1l , aes(Year, Yield, color = Crop)) + geom_line()
+
+### Geometries
+
+#### `geom_line`
+ggplot(dat1w, aes(x = Year, y = Oats)) + geom_line() 
+ggplot(dat1w, aes(x = Year, y = Oats)) + 
+   geom_line(linetype = 2, colour = "blue", size=0.4)
+
+#### `geom_point`
+ggplot(dat2, aes(x = B20004013, y = B20004007)) + geom_point(alpha = 0.3)
+ggplot(dat2, aes(x = B20004013, y = B20004007)) + 
+   geom_point(colour = "red", pch=3 , alpha = 0.3, cex=0.6)
+
+#### `geom_hex`
+library(hexbin)
+ggplot(dat2, aes(x = B20004013, y = B20004007)) + 
+   geom_hex(binwidth = c(1000, 1000))
+
+#### `geom_boxplot`
+ggplot(dat1l, aes(x = Crop, y = Yield)) + geom_boxplot(fill = "bisque") 
+ggplot(dat1l, aes(x = "", y = Yield)) + 
+   geom_boxplot(fill = "bisque") + xlab("All crops")
+
+#### `geom_violin`
+ggplot(dat1l, aes(x = "", y = Yield)) + geom_violin(fill = "bisque") 
+
+#### `geom_histogram`
+ggplot(dat1w, aes(x = Oats)) + geom_histogram(fill = "grey50")
+ggplot(dat1w, aes(x = Oats)) + 
+   geom_histogram(fill = "grey50", binwidth = 1000)
+ggplot(dat1w, aes(x = Oats)) + 
+   geom_histogram(fill = "grey50", bins = 8) 
+
+#### `geom_bar`
+library(forcats)
+ggplot(dat2, aes(State)) + geom_bar()
+ggplot(dat2, aes(fct_infreq(State,ordered = TRUE))) + geom_bar()
+ggplot(dat2, aes(fct_rev(fct_infreq(State,ordered = TRUE)))) + geom_bar()
+
+dat2.ct <- dat2 %>% group_by(State) %>% summarize(Counties = n())
+ggplot(dat2.ct, aes(x=fct_reorder(State, Counties, .fun = median), y = Counties)) + geom_bar(stat = "identity")
+
+#### dot plot
+ggplot(dat2.ct , aes(x = Counties, y = State)) + geom_point()
+ggplot(dat2.ct , aes(x = Counties, y = fct_reorder(State, Counties, .fun = median))) + geom_point()
+
+#### Combining geometries
+
+ggplot(dat2, aes(x = B20004013, y = B20004007)) + 
+   geom_point(alpha = 0.3, color='#4f4b4b') + 
+   geom_smooth(method = "lm", color='green')
+
+ggplot(dat2, aes(x = B20004013, y = B20004007)) + 
+   geom_point(alpha = 0.3, color='#4f4b4b') +
+   geom_smooth(method = "loess", color='green')
+
+### Tweaking a ggplot2 graph
+
+#### Plot title 
+ggplot(dat2, aes(State)) + geom_bar() + ggtitle("Number of counties by state")
+ggplot(dat2, aes(x = B20004013, y = B20004007)) + geom_point(alpha = 0.3) + xlab("Female income ($)") + ylab("Male income ($)")
+
+#### Axes labels
+ggplot(dat2, aes(x = B20004013, y = B20004007))+ 
+   geom_point(alpha = 0.3) +
+   xlab("Female income ($)") + 
+   ylab("Male income ($)") +
+   scale_x_continuous(breaks = c(10000, 30000, 50000),labels = c("$10,000", "$30,000", "$50,000"))
+
+ggplot(dat2, aes(x=B20004013, y=B20004007)) + geom_point(alpha=0.3) +
+   xlab("Female income ($)") + ylab("Male income ($)") +
+   scale_x_continuous(labels=scales::unit_format(suffix="k",scale=0.001, sep="")) +
+   scale_y_continuous(labels=scales::unit_format(suffix="k", scale=0.001, sep=""))
+
+ggplot(dat2, aes(x=B20004013, y=B20004007)) + geom_point(alpha=0.3) +
+   xlab("Female income ($)") + ylab("Male income ($)") +
+   scale_x_continuous(labels=scales::unit_format(suffix="k", scale=0.001, sep="")) +
+   scale_y_continuous(labels=scales::unit_format(suffix="k", scale=0.001, sep="")) +
+   theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
+
+#### Axes limits
+ggplot(dat2, aes(x = B20004013, y = B20004007)) + geom_point(alpha = 0.3) +
+   xlab("Female income ($)") + ylab("Male income ($)") +
+   xlim(10000, 75000) + ylim(10000, 75000)
+
+ggplot(dat2, aes(x = B20004013, y = B20004007)) + geom_point(alpha = 0.3) +
+   xlab("Female income ($)") + ylab("Male income ($)") +
+   scale_x_continuous(limit  = c(10000, 75000),labels = scales::comma_format()) +
+   scale_y_continuous(limit  = c(10000, 75000), labels = scales::comma_format())
+
+#### Axes breaks
+ggplot(dat2, aes(x = B20004013, y = B20004007)) + geom_point(alpha = 0.3) +
+   xlab("Female income ($)") + ylab("Male income ($)") +
+   scale_x_continuous(limit  = c(10000, 75000),labels = scales::comma_format(),breaks = c(10000, 30000, 50000, 70000)) +
+   scale_y_continuous(limit  = c(10000, 75000),labels = scales::comma_format(),breaks = c(10000, 30000, 50000, 70000))
+
+#### Axes and data transformations
+ggplot(dat2, aes(x = B20004013, y = B20004007)) + geom_point(alpha = 0.3) +
+   xlab("Female income ($)") + ylab("Male income ($)") +
+   coord_trans(x = "log")
+
+ggplot(dat2, aes(x = B20004013, y = B20004007)) + geom_point(alpha = 0.3) +
+   xlab("Female income ($)") + ylab("Male income ($)") +
+   coord_trans(x = scales::boxcox_trans(-0.3))
+
+ggplot(dat2, aes(x = B20004013, y = B20004007)) + geom_point(alpha = 0.3) +
+   geom_smooth(method = "lm", se = FALSE) +
+   xlab("Female income ($)") + ylab("Male income ($)") +
+   scale_x_continuous(trans = "log", breaks = seq(10000,60000,10000))
+
+#### Aspect ratio
+ggplot(dat2, aes(x = B20004013, y = B20004007)) + geom_point(alpha = 0.3) +
+   geom_smooth(method = "lm") +
+   xlab("Female income ($)") + ylab("Male income ($)") +
+   coord_equal(ratio = 1)
+
+#### Colors
+##### Continuous color schemes
+dat2.ct2 <- dat2 %>% group_by(State) %>% summarize(Counties = n(), Income = median(B20004001))
+ggplot(dat2.ct2, aes(x = fct_reorder(State, Counties), y = Counties, fill = Income)) +
+   geom_bar(stat = "identity") + 
+   scale_fill_gradient(low = "green", high = "red")
+
+
+ggplot(dat2.ct2, aes(x = fct_reorder(State, Counties), y = Counties, fill = Income)) +
+   geom_bar(stat = "identity") + 
+   scale_fill_gradient2(low = "darkred", mid = "white",  high = "darkgreen", midpoint = 30892)
+
+
+ggplot(dat2.ct2, aes(y = fct_reorder(State, Counties), x = Counties, col = Income)) +
+   geom_point() +
+   scale_colour_gradient2(low = "darkred", mid = "white",  high = "darkgreen", midpoint = 30892)
+
+##### Discrete color schemes
+ggplot(dat1l, aes(Year, Yield, col = Crop)) + 
+   geom_line() +
+   scale_colour_manual(values = c("red", "orange", "green", "blue", "yellow"))
+
+ggplot(dat1l, aes(Year, Yield, col = Crop)) + 
+   geom_line() +
+   scale_colour_brewer(palette = "Dark2")
+
+ggplot(dat1l, aes(Year, Yield, col = reorder(Crop, Yield, median))) + 
+   geom_line() +
+   guides(colour = guide_legend(title = "Crops")) +  
+   scale_colour_brewer(palette = "Reds") 
+
+#### Adding mathematical symbols to a plot
+ggplot(dat2, aes(x = B20004013^0.333, y = sqrt(B20004007))) + geom_point(alpha = 0.3) +
+   xlab( expression(("Female income") ^ frac(1,3))) + 
+   ylab( expression(sqrt("Male income")))
+
+### Faceting
+#### Faceting by categorical variables
+##### `facet_wrap`
+ggplot(dat1l2, aes(x = Year, y = Yield, color = Crop)) + geom_line() + 
+   facet_wrap( ~ Country, nrow = 1)
+
+ggplot(dat1l2, aes(x = Year, y=Yield)) + geom_line() +
+   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
+   facet_wrap(Crop ~ Country, nrow = 1)
+
+ggplot(dat1l2, aes(x = Year, y=Yield)) + geom_line() +
+   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
+   facet_wrap(Crop ~ Country, nrow = 1, labeller = label_wrap_gen(width = 12))
+
+##### `facet_grid`
+ggplot(dat1l2, aes(x = Year, y = Yield)) + geom_line() + 
+   facet_grid( Crop ~ Country)
+
+#### Faceting by continous variables (advance)
+dat2c$incrng <- cut(dat2c$F , breaks = c(0, 25000, 50000, 75000, 100000) )
+summary(dat2c$incrng)
+dat2c <- na.omit(dat2c)
+levels(dat2c$incrng)
+levels(dat2c$incrng) <-  c("Under 25k", "25k-50k", "50k-75k", "75k-100k")
+head(dat2c)
+ggplot(dat2c, aes(x = F, y = M)) + geom_point(alpha=0.2, pch=20) +
+   geom_smooth(method = "lm", col = "red") +
+   facet_grid( . ~ incrng) 
+
+### Exporting to an image
+p1 <- ggplot(dat1l2, aes(x = Year, y = Yield, color = Crop)) + geom_line() + 
+   facet_wrap( ~ Country, nrow = 1) +
+   scale_y_continuous(labels = scales::comma_format())
+ggsave("Output/fig0.png", plot = p1, width = 6, height = 2, units = "in", device = "png")
+
+p2 <- ggplot(dat1l2, aes(x = Year, y = Yield, color = Crop)) + geom_line() + 
+   facet_wrap( ~ Country, nrow = 1) +
+   scale_y_continuous(labels = scales::comma_format()) +
+   theme(axis.text    = element_text(size = 8, family = "mono"),
+         axis.title   = element_text(size = 11, face = "bold"),
+         strip.text   = element_text(size = 11, face="italic", family = "serif"),
+         legend.title = element_text(size = 10, family = "sans"),
+         legend.text  = element_text(size = 8,  color = "grey40"))
+ggsave("Output/fig1.png", plot = p2, width = 6, height = 2, units = "in")
 
